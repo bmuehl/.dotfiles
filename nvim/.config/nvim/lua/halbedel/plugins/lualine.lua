7a8a24c097bf464ce0dfc23538aa49f047693739
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-11-17 23:08:43 +0100

    show mac icon in lualine

--- nvim/.config/nvim/lua/halbedel/plugins/lualine.lua
+++ nvim/.config/nvim/lua/halbedel/plugins/lualine.lua
@@ -1,16 +1,33 @@
-
 -- import lualine plugin safely
 local status, lualine = pcall(require, "lualine")
 if not status then
-  return
+	return
 end
 
 -- get lualine theme
 local theme = require("lualine.themes.nord")
 
+local function os_icon()
+	local icons = {
+		unix = "", -- e712
+		dos = "", -- e70f
+		mac = "", -- e711
+	}
+	if vim.fn.has("mac") == 1 then
+		return icons.mac
+	elseif vim.fn.has("win32") == 1 then
+		return icons.dos
+	else
+		return icons.unix
+	end
+end
+
 -- configure lualine with modified theme
 lualine.setup({
-  options = {
-    theme = theme,
-  },
+	options = {
+		theme = theme,
+	},
+	sections = {
+		lualine_x = { "encoding", os_icon, "filetype" },
+	},
 })
