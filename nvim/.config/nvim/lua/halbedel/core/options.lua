77abd61bd6e4341130a8bd552037e3a516b351fd
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-11-28 01:58:53 +0100

    Don't expand tabs to spaces

--- nvim/.config/nvim/lua/halbedel/core/options.lua
+++ nvim/.config/nvim/lua/halbedel/core/options.lua
@@ -10,7 +10,7 @@ opt.mouse = "a"
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+-- opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
