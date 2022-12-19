2cf7a9b41c41e2b40fe692ec9e6568cf3d37a25f
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-12-19 16:30:44 +0100

    fix prettier

--- nvim/.config/nvim/lua/halbedel/plugins/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/halbedel/plugins/lsp/null-ls.lua
@@ -17,7 +17,7 @@ null_ls.setup({
 	sources = {
 		--  to disable file types use
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-		formatting.prettierd.with({
+		formatting.prettier.with({
 			filetypes = {
 				"html",
 				"json",
