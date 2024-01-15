3042e10c43795fb8dd4ffb6467dd945246f69dc4
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-01-15 15:14:52 +0100

    fix diagnostic symbols

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -86,8 +86,8 @@ return {
 		})
 
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+		-- find symbols here: https://github.com/folke/trouble.nvim
+		local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
