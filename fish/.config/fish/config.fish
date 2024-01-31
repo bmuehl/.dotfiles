50b13b09f625725bb77f4244879d0c7b700ae352
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-01-31 15:56:27 +0100

    add tmux command to swap windows

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,6 +18,10 @@ if status is-interactive
 	# Commands to run in interactive sessions can go here
 	fish_vi_key_bindings
 end
+ 
+# golang
+set -gx GOPATH $HOME/go
+set -gx PATH $PATH $GOPATH/bin
 
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"
