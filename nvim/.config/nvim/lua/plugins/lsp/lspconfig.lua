163275603662e5a347022469db006f445ad97115
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-02-12 17:16:01 +0100

    change lsp keymap

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -92,14 +92,14 @@ return {
 					vim.keymap.set(mode, lhs, rhs, opts)
 				end
 
-				bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")             -- Displays hover information about the symbol under the cursor
-				bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")       -- Jump to the definition
-				bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")      -- Jump to declaration
-				bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")   -- Lists all the implementations for the symbol under the cursor
-				bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")  -- Jumps to the definition of the type symbol
-				bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")       -- Lists all the references
-				bufmap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>") -- Displays a function's signature information
-				bufmap("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>")    -- Renames all references to the symbol under the cursor
+				bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")              -- Displays hover information about the symbol under the cursor
+				bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")        -- Jump to the definition
+				bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")       -- Jump to declaration
+				bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")    -- Lists all the implementations for the symbol under the cursor
+				bufmap("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>")   -- Jumps to the definition of the type symbol
+				bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")        -- Lists all the references
+				bufmap("n", "<Leader>k", "<cmd>lua vim.lsp.buf.signature_help()<cr>") -- Displays a function's signature information
+				bufmap("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>")     -- Renames all references to the symbol under the cursor
 				bufmap("n", "<Leader>a", "<cmd>lua vim.lsp.buf.code_action()<cr>") -- Selects a code action available at the current cursor position
 				bufmap("x", "<Leader>a", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
 				bufmap("n", "<Leader>d", "<cmd>lua vim.diagnostic.open_float()<cr>") -- Show diagnostics in a floating window
