d8e12bcd519b2d0b196a3e74bac503fe8f7b6e54
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-04-15 10:14:51 +0200

    add keymapping for find in cwd

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -8,6 +8,8 @@ return {
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
+		local builtin = require("telescope.builtin")
+		local utils = require("telescope.utils")
 
 		telescope.setup({
 			pickers = {
@@ -20,14 +22,18 @@ return {
 				file_ignore_patterns = { "node_modules", "target", ".git" },
 				mappings = {
 					i = {
-						["<C-k>"] = actions.move_selection_previous,                 -- move to prev result
-						["<C-j>"] = actions.move_selection_next,                     -- move to next result
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-						["<C-o>"] = actions.send_to_qflist + actions.open_qflist,    -- send all to quickfixlist
+						["<C-o>"] = actions.send_to_qflist + actions.open_qflist, -- send all to quickfixlist
 					},
 				},
 			},
 		})
+
+		vim.keymap.set("n", "<leader>fd", function()
+			builtin.live_grep({ search_dirs = { utils.buffer_dir() } })
+		end) -- find string in current buffer directory as you type
 
 		telescope.load_extension("fzf")
 	end,
