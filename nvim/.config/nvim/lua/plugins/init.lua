1ba1b926eb2877107fa313690890b2b4288c8df9
Bernhard Mühl <bernhard.muehl@willhaben.at>
2024-10-28 15:40:30 +0100

    add syntax highlights for dbml

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
-	"tpope/vim-surround",             -- add, delete, change surroundings ysw)
+	"tpope/vim-surround", -- add, delete, change surroundings ysw)
 	"vim-scripts/ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
-	"terryma/vim-multiple-cursors",   -- multiple cursors with CTRL + N
-	"ap/vim-css-color",               -- CSS color preview,
-	"windwp/nvim-ts-autotag",         -- autoclose tags
-	"mfussenegger/nvim-jdtls",        -- java lsp
+	"terryma/vim-multiple-cursors", -- multiple cursors with CTRL + N
+	"ap/vim-css-color", -- CSS color preview,
+	"windwp/nvim-ts-autotag", -- autoclose tags
+	"mfussenegger/nvim-jdtls", -- java lsp
 	"christoomey/vim-tmux-navigator", -- navigate between vim and tmux panes
-	"editorconfig/editorconfig-vim",  -- editorconfig
+	"editorconfig/editorconfig-vim", -- editorconfig
+	"jidn/vim-dbml", -- dbml
 }
