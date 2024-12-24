8681da503980c412e691993dfca9ad072427b7b5
Bernhard Mühl <bernhard.muehl@Bernhards-MacBook-Pro.local>
2024-12-24 15:51:21 +0100

    httpyac add keymap for prod env

--- nvim/.config/nvim/lua/plugins/nvim-httpyac.lua
+++ nvim/.config/nvim/lua/plugins/nvim-httpyac.lua
@@ -5,5 +5,6 @@ return {
 		-- if you want to set up the keymaps
 		vim.keymap.set("n", "<Leader>x", "<cmd>:NvimHttpYac<CR>", { desc = "Run request" })
 		vim.keymap.set("n", "<Leader>xx", "<cmd>:NvimHttpYacAll<CR>", { desc = "Run all requests" })
+		vim.keymap.set("n", "<Leader>xp", "<cmd>:NvimHttpYac --env prod<CR>", { desc = "Run request with prod env" })
 	end,
 }
