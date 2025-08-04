e1c726ea4e14aa30d64d0e6698d18f3737cc3eb8
Bernhard Mühl <bernhard.muehl@willhaben.at>
2025-08-04 21:26:25 +0200

    extract variables from fish_variables to separate files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,34 +1,19 @@
-# abbreviations
-abbr -a gc --position anywhere --set-cursor 'git commit -m "%"'
-abbr -a ga --position anywhere "git add -A"
-abbr -a gp --position anywhere "git push"
-abbr -a lg --position anywhere "lazygit"
+# Fish shell configuration
 
-# kubectl
+# kubectl completion
 kubectl completion fish | source
 
 if status is-interactive
-	# Commands to run in interactive sessions can go here
-	fish_vi_key_bindings
-	bind -M insert -m default jk backward-char force-repaint # switch to command mode with jk
+    # Commands to run in interactive sessions can go here
+    fish_vi_key_bindings
+    bind -M insert -m default jk backward-char force-repaint # switch to command mode with jk
 end
- 
-# golang
-set -gx GOPATH $HOME/go
-set -gx PATH $PATH $GOPATH/bin
 
-# pnpm
-set -gx PNPM_HOME "$HOME/Library/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-# pnpm end
-
-set -gx XDG_CONFIG_HOME "$HOME/.config"
-set -gx LANG "en_US.UTF-8"
-set -gx LC_ALL "en_US.UTF-8"
-set -gx LC_CTYPE "en_US.UTF-8"
-
-set -gx PHP_CS_FIXER_IGNORE_ENV true # delete after php-cs-fixer update
-
+# System configuration
 ulimit -n 100000 # increase max open files
+
+# Add paths 
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/opt/python/libexec/bin
+fish_add_path $GOPATH/bin
+fish_add_path "$PNPM_HOME"
