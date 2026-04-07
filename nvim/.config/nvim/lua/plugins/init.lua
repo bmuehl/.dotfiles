6e33d996540e5fe78215729af7f2f0914038865b
Bernhard Mühl <bernhard.muehl@gmail.com>
2026-04-07 17:07:45 +0200

    replace vim-css-color

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,7 +2,11 @@ return {
 	"tpope/vim-surround", -- add, delete, change surroundings ysw)
 	"vim-scripts/ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 	"terryma/vim-multiple-cursors", -- multiple cursors with CTRL + N
-	"ap/vim-css-color", -- CSS color preview,
+	{
+		"catgoose/nvim-colorizer.lua",
+		event = "BufReadPre",
+		opts = {},
+	},
 	"windwp/nvim-ts-autotag", -- autoclose tags
 	"mfussenegger/nvim-jdtls", -- java lsp
 	"christoomey/vim-tmux-navigator", -- navigate between vim and tmux panes
